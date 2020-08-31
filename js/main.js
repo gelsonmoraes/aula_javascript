662151874b35e1ba913b4ac0ae72e4127660d25d
GelsonMoraes <gelsonluizmoraes@gmail.com>
2020-08-31 09:16:08 -0300

    Funcionalidades do JavaScript

--- js/main.js
+++ js/main.js
@@ -7,7 +7,33 @@ alert("Bem-vindo, " + nome + "!");
 var idade = prompt("Qual é a sua idade?");
 
 if(idade < 18){
-    alert(nome + ", você tem " + idade + " anos e é menor de idade em " + d.getDate() +"/"+ d.getMonth() +"/" + d.getFullYear() + "!")
+    alert(nome + ", você tem " + idade + " anos e é menor de idade em " + d.getDate() +"/"+ (d.getMonth()+1) +"/" + d.getFullYear() + "!")
 }else{
-    alert(nome + ", você tem " + idade + " anos e é maior de idade em " + d.getDay() +"/"+ d.getMonth() +"/" + d.getFullYear() + "!")
+    alert(nome + ", você tem " + idade + " anos e é maior de idade em " + d.getDate() +"/"+ (d.getMonth()+1) +"/" + d.getFullYear() + "!")
+}
+
+
+function clicou(){
+    
+    document.getElementById("agradecimento").innerHTML =  "Obrigado por clicar";
+    //alert("Obrigado por clicar!");
+    
+}
+
+function redirecionar(){
+    window.open("https://www.linkedin.com/in/gelson-moraes-ads/");
+}
+
+function trocar(elemento){
+    //document.getElementById("passamouse").innerHTML = "Obrigado por passar o mouse!";
+    elemento.innerHTML =  "Obrigado por passar o mouse!";
+}
+
+function voltar(elemento){
+    //document.getElementById("passamouse").innerHTML = "Passe o mouse aqui!"
+    elemento.innerHTML =  "Passe o mouse aqui!";
+}
+
+function funcaoChange(elemento){
+    alert("Você selecionou o valor " + elemento.value + "!")
 }
